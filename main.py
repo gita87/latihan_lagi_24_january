21d2f2d85dbfc74f51c64cf46e54189d69908c1c
Gita87 <gitasuputra@gmail.com>
2021-01-18 16:21:04 +0700

    coba lagi latihan commit "perulangan"

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print('hello coy')
 print('test lagi dan lagi')
+print('___'*9)
+
+jumlah_anak = 4
+
+for index in range(1,5):
+    print(f'halo anak {index}')
+
+
