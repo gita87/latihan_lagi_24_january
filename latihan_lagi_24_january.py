7bd75b3d29dbd9f9c8728b1bb1cd536c3153c7ba
Gita87 <gitasuputra@gmail.com>
2021-01-24 15:03:29 +0700

    latihan 24 january bagian 2

--- latihan_lagi_24_january.py
+++ latihan_lagi_24_january.py
@@ -1,1 +1,18 @@
 print('hello boy')
+print('___' * 8)
+
+print('\nbelajar percabangan')
+
+ingin_cepat = True
+if ingin_cepat:
+    print('\njalan lurus')
+else:
+    print('jalan mundur')
+
+print('\ntest lagi')
+
+main_game = False
+if main_game:
+    print('\n libur')
+else:
+    print('\n masuk kerja')
