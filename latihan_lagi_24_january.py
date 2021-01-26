a3e76ede9284a45648dce92a98aa6ef767339225
Gita87 <gitasuputra@gmail.com>
2021-01-26 17:46:12 +0700

    latihan 24 january bagian 2 latihan 25 january fungsi sort, dan update

--- latihan_lagi_24_january.py
+++ latihan_lagi_24_january.py
@@ -16,3 +16,36 @@ if main_game:
     print('\n libur')
 else:
     print('\n masuk kerja')
+
+print('\n coba lagi')
+
+istri_2 = False
+if istri_2:
+    print('\nnafsu')
+else:
+    print('\nmata keranjang')
+print('\nbelajar perulangan')
+
+jumlah_anak = 10
+for index_anak in range(2, jumlah_anak + 3):
+    print(f'halo anak {index_anak}')
+
+menu_makanan = ['nasi goreng', 'gado-gado', 'pete', 'lemper', 'bakso']
+print(len(menu_makanan))
+
+luas = [10, 15, 20, 25, 30, 35, 50]
+print(min(luas))
+print()
+angka = [12,56,34,10,45]
+angka.sort()
+print(angka)
+
+print()
+r = {'lemon':'kagura', 'R7':'chou', 'tuturu':'bart', 'albert': 'cloude'}
+print(r)
+
+k = {'xinn':'ling'}
+print(k)
+print()
+r.update(k)
+print(r)
